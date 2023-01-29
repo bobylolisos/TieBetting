8722a94eb719962f7edde40e3b40927d21e3995e
Marcus Bylsäter <marcus.bylsater@knowit.se>
2023-01-29 12:32:23 +0100

    Hide "Amount to bet:" when match is active

---
+++ TieBetting/Converters/MatchStatusToVisibilityConverter.cs
@@ -0,0 +1,27 @@
+﻿namespace TieBetting.Converters;
+
+public class MatchStatusToVisibilityConverter : ValueConverterBase<MatchStatus>
+{
+    public bool NotActiveVisibility { get; set; } = false;
+    public bool ActiveVisibility { get; set; } = false;
+    public bool LostVisibility { get; set; } = false;
+    public bool WinVisibility { get; set; } = false;
+
+
+    protected override object Convert(MatchStatus value, object parameter)
+    {
+        switch (value)
+        {
+            case MatchStatus.NotActive:
+                return NotActiveVisibility;
+            case MatchStatus.Active:
+                return ActiveVisibility;
+            case MatchStatus.Lost:
+                return LostVisibility;
+            case MatchStatus.Win:
+                return WinVisibility;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(value), value, null);
+        }
+    }
+}
