00de6440e8164fefb83db85c15ebcf4d91520517
Marcus Bylsäter <marcus.bylsater@knowit.se>
2023-02-09 13:43:25 +0100

    Added leage images SHL & HA

---
+++ TieBetting/Converters/LeagueToImageConverter.cs
@@ -0,0 +1,26 @@
+﻿using System.Resources;
+
+namespace TieBetting.Converters;
+
+public class LeagueToImageConverter : ValueConverterBase<string>
+{
+    protected override object Convert(string value, object parameter)
+    {
+        if (value == null)
+        {
+            return null;
+        }
+
+        if (value.ToLower().Contains("hockeyallsvenskan"))
+        {
+            return "ha.png";
+        }
+
+        if (value.ToLower().Contains("shl"))
+        {
+            return "shl.png";
+        }
+
+        return null;
+    }
+}
