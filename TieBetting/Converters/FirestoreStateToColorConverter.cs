15a08232853ecdba9c568bc7ccf3a97ce6a90a0e
Marcus Bylsäter <marcus.bylsater@knowit.se>
2023-09-07 14:34:19 +0200

    Visualizing sandbox mode

--- TieBetting/Converters/FirestoreStateToColorConverter.cs
+++ TieBetting/Converters/FirestoreStateToColorConverter.cs
@@ -1,12 +1,17 @@
 ﻿namespace TieBetting.Converters;
 
-public class FirestoreStateToTextColorConverter : IValueConverter
+public class FirestoreStateToColorConverter : IValueConverter
 {
     public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
     {
         if (FirestoreRepository.SandBox)
         {
             return Colors.Red;
+        }
+
+        if (App.Current.Resources.TryGetValue("Primary", out var colorvalue))
+        {
+            return (Color)colorvalue;
         }
 
         return Colors.White;
